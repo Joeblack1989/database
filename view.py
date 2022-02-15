0b10293641ff97985d9476664100a2a76578ceeb
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-02-16 00:14:50 +0300

    Add view and additional

---
+++ view.py
@@ -0,0 +1,9 @@
+
+def get_request():
+    frst_choice = int(input ('Выберите действие: \n'
+                    '1 - внести изменения в список\n'
+                    '2 - вывести данные\n'))
+    sec_choice = int(input())
+    return frst_choice
+
+get_request()
