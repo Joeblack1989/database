209baba4ca3cf92bd58af683d9265c071ba824ca
NataSola <nsol2009@yandex.ru>
2022-02-16 16:48:47 +0300

    Add method

--- view.py
+++ view.py
@@ -1,14 +1,26 @@
 
+from additional import data_proc as dp, msg
+
 def get_request():
     frst_choice = int(input ('Выберите действие: \n'
                     '1 - внести изменения в список\n'
                     '2 - вывести данные\n'))
-    sec_choice = int(input())
-    return frst_choice
+    # for key, value in msg.items:
+    #     print(f'{key} - {value}')
+        # print(f'{msg[frst_choice]}.keys() - {msg[frst_choice][i]}.values()\n')
+
+    for f, k in msg[frst_choice].items():
+        print(f'\t{f} - {k} ')
+
+
+    sec_choice = int(input())  
+    # sec_choice = int(input(msg[frst_choice]))
+    # func = data_proc[frst_choice][sec_choice]
+    return dp[frst_choice][sec_choice]
+
+# print(get_request())
 
 get_request()
-
-
 f_add = 'add'
 f_del = 'del'
 f_all = 'all'
