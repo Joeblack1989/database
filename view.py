f4e4b21a94f2aee7a4b6515cc80a258596518b93
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-02-16 23:58:23 +0300

    add UI

--- view.py
+++ view.py
@@ -1,29 +1,18 @@
+# import os
 
-from additional import data_proc as dp, msg
+import additional as ad
+
+# from database import path_txt as pt, data as dt
 
 def get_request():
     frst_choice = int(input ('Выберите действие: \n'
-                    '1 - внести изменения в список\n'
-                    '2 - вывести данные\n'))
-    # for key, value in msg.items:
-    #     print(f'{key} - {value}')
-        # print(f'{msg[frst_choice]}.keys() - {msg[frst_choice][i]}.values()\n')
-
-    for f, k in msg[frst_choice].items():
+                    '\t1 - внести изменения в список\n'
+                    '\t2 - вывести данные\n'))
+    for f, k in ad.msg[frst_choice].items():
         print(f'\t{f} - {k} ')
-
-
     sec_choice = int(input())  
-    # sec_choice = int(input(msg[frst_choice]))
-    # func = data_proc[frst_choice][sec_choice]
-    return dp[frst_choice][sec_choice]
+    return ad.data_proc[frst_choice][sec_choice]
 
-# print(get_request())
+# func = get_request()
 
-get_request()
-f_add = 'add'
-f_del = 'del'
-f_all = 'all'
-f_class = 'class'
-f_id = 'id'
-
+print(get_request())
